baa3bce9ae01ee7e679625cc0a911b775315b60c
Aldo Matias <aldo_rheazyk@hotmail.com>
2022-01-13 21:26:30 -0600

    [FIX] Refactor functionality CRUD

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -31,9 +31,24 @@ const Container = () => {
       },
       parent: 0,
     },
+    {
+      id: "3",
+      title: "TAREA 3",
+      status: 0,
+      type: 0,
+      description: "",
+      assignedto: {
+        userId: "",
+        name: "",
+        lastname: "",
+      },
+      parent: 0,
+    },
   ]);
 
   const [todo, setTodo] = useState([]);
+  const [blocked, setBlocked] = useState([]);
+  const [inprogress, setInprogress] = useState([])
 
   const reorder = (requirement, startIndex, endIndex) => {
     const result = Array.from(requirement);
@@ -42,52 +57,97 @@ const Container = () => {
     return result;
   };
 
-  const dragEndTodo = (result, source) => {
-    console.log(result);
 
-    let removed; 
-    switch (source) {
-      case 'todo':
-        [removed] = todo.splice(result.source.index, 1);
-        setRequirement(prevState=> [...prevState, removed]);
-        break;
-      case 'requirement':
-        [removed] = requirement.splice(result.source.index, 1);
-        setTodo(prevState=> [...prevState, removed]);
-        break;
-        default:
+  const crud = {
+
+    requirement: {
+      add: (element) => {
+        setRequirement(prevState=> [...prevState, element]);
+      },
+      remove: (element) => {
+        const [removed] = requirement.splice(element.source.index, 1);
+        return removed;
+      }
+    },
+    todo: {
+      add: (element) => {
+        setTodo(prevState=> [...prevState, element]);
+      },
+      remove: (element) => {
+        const [removed] = todo.splice(element.source.index, 1);
+        return removed;
+      }
+    },
+    blocked: {
+      add: (element) => {
+        setBlocked(prevState=> [...prevState, element]);
+      },
+      remove: (element) => {
+        const [removed] = blocked.splice(element.source.index, 1);
+        return removed;
+      }
+    },
+    inprogress: {
+      add: (element) => {
+        setInprogress(prevState=> [...prevState, element]);
+      },
+      remove: (element) => {
+        const [removed] = inprogress.splice(element.source.index, 1);
+        return removed;
+      }
     }
-    
-
   }
 
   const onDragEnd = (result) => {
     if (!result.destination) {
         return;
     }
-
-    switch (result.destination.droppableId) {
-        case "todo":
-          dragEndTodo(result, result.source.droppableId);
-        break;
-        case "requirement":
-          dragEndTodo(result, result.source.droppableId);
-        break;
-        default:
-          console.log("No droppableId");
-        break;
-    }
-
-    
+    console.log(result.source.droppableId);
+    console.log(crud[result.source.droppableId]);
+    const removed = crud[result.source.droppableId].remove(result);
+    crud[result.destination.droppableId].add(removed);
   };
+  
+  const handleAddElement = (e) => {
+    console.log(e);
+    e.preventDefault();
+    const value = document.getElementById("add").value;
+    
+    if (!value) {
+      return;
+    }
+    
+    const request = {
+      id: Math.random()+"",
+      title: value,
+      status: 0,
+      type: 0,
+      description: "",
+      assignedto: {
+        userId: "",
+        name: "",
+        lastname: "",
+      },
+      parent: 0,
+    }
+    crud.requirement.add(request);
+
+  }
+  
 
   return (
     <div >
       <h1>TO DO LIST</h1>
+      <input type="text" placeholder="ADD" id="add"/>
+      <button onClick={handleAddElement}>ADD</button>
+
+
       <div className="d-flex">
       <DragDropContext onDragEnd={onDragEnd}>
         <Column data={requirement} droppableId="requirement"/>
         <Column data={todo} droppableId="todo"/>
+        <Column data={blocked} droppableId="blocked"/>
+        <Column data={inprogress} droppableId="inprogress"/>
       </DragDropContext>
 
       </div>
